1c451529b5e5a5cd98735899e96f58abb0f1c1ea
GraziosoG <grace.chiu25@utexas.edu>
2025-12-21 10:55:25 -0600

    Upgraded responsive design

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 let now_playing = document.querySelector('.now-playing');
 let track_art = document.querySelector('.track-art');
 let track_name = document.querySelector('.track-name');
-let track_artist = document.querySelector('.track-artist');
 
 let playpause_btn = document.querySelector('.playpause-track');
 let next_btn = document.querySelector('.next-track');
