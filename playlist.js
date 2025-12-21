ee04c77ee80f2de70a3fce2775ea89886785a9c8
GraziosoG <grace.chiu25@utexas.edu>
2025-12-20 22:48:14 -0600

    Completed with song track

---
+++ playlist.js
@@ -0,0 +1,27 @@
+export const music_list = [
+    {
+        name: "Winter Wonderland",
+        img: "multiple-trees.jpg",
+        audio: "Winter-Wonderland.mp3"
+    }, 
+    {
+        name: "Have a Holly Jolly Christmas",
+        img: "giftbox.jpg",
+        audio: "Holly-Jolly-Chirstmas.mp3"
+    }, 
+    {
+        name: "Let it Snow",
+        img: "car.jpg",
+        audio: "Let-It-Snow.mp3"
+    }, 
+    {
+        name: "White Christmas",
+        img: "xmas-tree-night.jpg",
+        audio: "White-Christmas.mp3"
+    }, 
+    {
+        name: "We Wish You a Merry Christmas",
+        img: "tree.jpg",
+        audio: "We-Wish-You-a-Merry-Christmas.mp3"
+    }
+];
